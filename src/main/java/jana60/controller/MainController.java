e72410ec19b3848085b84f333222baf63dde2ceb
matteocovetti <covetti.matteo@gmail.com>
2022-07-12 15:24:44 +0200

    Bonus completato

--- src/main/java/jana60/controller/MainController.java
+++ src/main/java/jana60/controller/MainController.java
@@ -1,6 +1,10 @@
 package jana60.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +15,14 @@ public class MainController {
 	@GetMapping("/home")
 	public String saluto() {
 		return "home";
+	}
+
+	@GetMapping("/ora")
+	public String orario(Model model) {
+		LocalDateTime data = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		String dataFormattata = data.format(formatter);
+		model.addAttribute("data", dataFormattata);
+		return "ora";
 	}
 }
