41a9d718492b2010e8710707bf0a9bd111ac9ea6
matteocovetti <covetti.matteo@gmail.com>
2022-07-12 14:57:51 +0200

    first commit

---
+++ src/main/java/jana60/controller/MainController.java
@@ -0,0 +1,15 @@
+package jana60.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class MainController {
+
+	@GetMapping("/home")
+	public String saluto() {
+		return "home";
+	}
+}
